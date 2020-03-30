625521f8a26a92e77c3df646f39b45401bad2246
DogSniffers <DogSniffers@users.noreply.github.com>
2020-03-30 12:02:39 -0600

    animations work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Title from './Components/Title/Title'
+import Function from './Components/Function/Function'
 
 function App() {
   return (
     <div>
       <h1>Competencies:</h1>
       <Title/>
+      <Function/>
     </div>
   );
 }
---
+++ src/Components/Function/Function.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+function Function(props){
+    return <div>Functional Component ya 'herd?</div>
+}
+export default Function
