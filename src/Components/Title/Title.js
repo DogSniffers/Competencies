0883eeebf65c5ed167a13cf4697c5559202b9446
DogSniffers <DogSniffers@users.noreply.github.com>
2020-04-02 08:50:57 -0600

    p

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -5,14 +5,19 @@ class Title extends React.Component{
     constructor(){
         super()
         this.state = {
-
+            buttonClicked:false,
         }
     }
     render(){
         return(
             <div>
-                <button id='dog'>Click me before you can continue to the next page</button>
+                <button id='dog' onClick={() => this.setState({buttonClicked:!this.state.buttonClicked})}>Click me before you can continue to the next page</button>
                 <button className='dog'>Click me after you click the other button</button>
+                {this.state.buttonClicked === true ? (
+                    <div>Button Clicked</div>
+                ):(
+                    <div>Button Not Clicked</div>
+                )}
                 <h1 className='fixed'>FIXED EXAMPLE</h1>
             </div>
         )
